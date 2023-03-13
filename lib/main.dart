e0ed1b61f004fd5584428b392977ec2122701275
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-03-13 20:45:12 +0000

    feat: adding sized box with device independant pixels

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center, 
           children: [
             BigCard(pair: pair),  
+            SizedBox(height: 10,),
             ElevatedButton(
               onPressed: () {
                 appState.getNext();
